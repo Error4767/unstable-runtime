98d9f4608e3124c47d08fabca5056b2d0267f653
error4767 <2303184060@qq.com>
2022-08-19 13:52:14 +0800

    support SwitchStatement

--- src/statementHandlers/index.js
+++ src/statementHandlers/index.js
@@ -35,7 +35,7 @@ function executeBody(t, scopes) {
                 stackInfo.returned = true;
                 stackInfo.returnValue = execute(stmt.argument, scopes);
             } catch (err) {
-                throw new Error("找不到要 return 的函数");
+                throw new SyntaxError("Illegal return statement");
             }
             return true;
         }
@@ -44,7 +44,7 @@ function executeBody(t, scopes) {
                 // break 标记
                 loopScopeInfo.breaked = true;
             } catch (err) {
-                throw new Error("找不到要 break 的循环");
+                throw new SyntaxError("Illegal break statement");
             }
             return true;
         } else if (stmt.type === "ContinueStatement") {
@@ -52,7 +52,7 @@ function executeBody(t, scopes) {
                 // continue 标记
                 loopScopeInfo.continued = true;
             } catch (err) {
-                throw new Error("找不到要 continue 的循环");
+                throw new SyntaxError("Illegal continue statement: no surrounding iteration statement");
             }
             return true;
         }
@@ -156,4 +156,80 @@ export default {
             }
         }
     },
+    "SwitchStatement": (t, scopes)=> {
+        /* 
+            1. switch 的行为，一旦有一个 case 成功，那么就会执行其和其后 case 的动作，并且之后的 case 的动作不会触发条件检测，会直接执行动作，使用 break 退出 switch 语句
+            switch default 的行为: 
+                如果 default 在末尾，那么当其他所有 case 不匹配时，default 将执行
+                如果 default 不在末尾
+                    如果其他 case 中存在匹配成功的，则从那个成功的开始，是 1 对应的动作，故如果 default 在前，匹配成功的 case 在后，则不会执行 default，会跳过，从匹配成功的开始执行，如果在后，则不影响执行，和 1 一样，后续的 case 无视条件，所以会被执行，除非 break
+                    如果其他 case 中不存在成功的，则会从 default 开始执行，以及 default 之后的 case, 同 1 动作一样，无视条件，可以理解为将 default 视为一个成功的 case
+
+            上述解释是 switch 的一些行为，这可以帮助理解下面的代码为何如此执行，使得其符合 JS 标准
+        */
+        
+        // 需要匹配的值
+        const matchValue = execute(t.discriminant, scopes);
+
+        // 创建 switch 主体作用域
+        const scope = createScope();
+        const newScopes = [...scopes, scope];
+        // 由于break 和循环相近，便于实现这里直接设置其为循环，赋予其循环的性质
+        loopScopesInfos.set(scope, { breaked: false, continued: false });
+
+        // 如果 default 数量超过1，报错 
+        const defaultNumber = t.cases?.reduce((o, n)=> {
+            return  !n.test ? o + 1 : o;
+        }, 0);
+        if(defaultNumber > 1) {
+            throw new SyntaxError("More than one default clause in switch statement");
+        }
+
+        // 运行 case 的方法
+        const run = (switchCaseItem)=> {
+            // 判断成功并且有对应动作代码就执行，这里直接 executeBody，因为 case 属于 switch 主体的那个作用域，不需要再次创建一个作用域
+            switchCaseItem.consequent && executeBody({body: switchCaseItem.consequent}, newScopes);
+            // 检查标记break，和当前栈是否 return, 如果是，结束遍历执行
+            if (loopScopesInfos.get(scope)?.breaked || stackInfos.get(findStack(scopes))?.returned) {
+                return true;
+            }
+        }
+
+        // 已经匹配成功过了，switch 只要匹配一次成功，剩余的匹配就会不再执行
+        let isTestSucceed = false;
+        // 从 default 开始截取的 cases
+        let splitedCasesStartFromDefualt = null;
+        // 执行 switch 主体匹配
+        t.cases?.some((switchCaseItem, index)=> {
+            // 如果已经成功过了，直接运行代码
+            if(isTestSucceed) {
+                return run(switchCaseItem);
+            }
+            if(switchCaseItem.test) {
+                // 执行代码, 如果已经成功过，那么直接跳过判断，否则检测 test 表达式
+                const testResult = isTestSucceed || execute(switchCaseItem.test, newScopes) === matchValue;
+                // 判断成功
+                if(testResult) {
+                    // 已匹配过为false, 那么就设置为已匹配过
+                    !isTestSucceed && (isTestSucceed = true);
+                    return run(switchCaseItem);
+                }
+                // 不成功，则不做动作
+            }else {
+                // 如果是最后一个，则直接运行，不是，则先记录
+                if(index === t.cases.length - 1) {
+                    // 这里设置为 true 以便下面不在运行
+                    isTestSucceed = true;
+                    return run(switchCaseItem);
+                }else {
+                    // 存储从 default 开始的 switch
+                    splitedCasesStartFromDefualt = t.cases.slice(index);
+                }
+            }
+        });
+        // 如果 default 以外其他的没有匹配成功的，那么以 default 作为第一个成功的执行其以及其后的所有 case
+        if(!isTestSucceed) {
+            splitedCasesStartFromDefualt.some(switchCaseItem=> run(switchCaseItem));
+        }
+    }
 }
